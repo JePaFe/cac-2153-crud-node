f0a2ddb82a65918ae409c6942d96cc65b8070ed0
Jean Paul Ferreira <jeanpaul@jepafe.net>
2021-11-11 21:13:13 -0300

    Clase 29

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 
-app.use('/', require('./router'))
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', require('./routes/productos'))
+app.use('/', require('./routes/contacto'))
 
 app.use((req, res, next) => {
     res.status(404).send('Not found')
