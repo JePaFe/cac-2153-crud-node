d1ee9ea440cc20c58212d1a72340d8f084527c23
Jean Paul Ferreira <jeanpaul@jepafe.net>
2021-11-16 21:07:44 -0300

    Clase 30

--- routes/productos.js
+++ routes/productos.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const productos = require('../productos')
+const controller = require('../controller')
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/productos', (req, res) => {
-    // console.log(productos.all())
-    res.render('productos/index', { productos: productos.all() })
-})
-
-router.get('/productos/:nro', (req, res) => {
-    res.render('productos/show', { producto: productos.find(req.params.nro) })
-})
+router.get('/productos', controller.index)
+router.get('/productos/:nro', controller.show)
 
 module.exports = router
